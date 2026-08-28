776f4d1fe20839a76fcdd8a98e2b0f7dcbeab0de
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-28 20:22:36 -0300

    Apply exact original LEXOFFICE layout override

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 import './legacy-auth.css';
+import './original-layout.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
