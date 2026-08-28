135ad5e924590b377fcd49cad07fd9e75b758d99
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-28 16:21:05 -0300

    Load Suzanne Figueiredo table branding

--- src/main.tsx
+++ src/main.tsx
@@ -5,5 +5,6 @@ import App from './App';
 import './styles.css';
 import './legacy-auth.css';
 import './reference-ui.css';
+import './sf-table-brand.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
