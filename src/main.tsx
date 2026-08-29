fd0deb2eb07863052c1acfe4aadc87b109c96999
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-29 19:26:17 -0300

    Enable visual accessibility stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './interaction-feedback.css';
 import './office-brand.css';
 import './module-fixes.css';
 import './screen-fixes.css';
+import './accessibility-vision.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
