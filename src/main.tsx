79aad8921fce2c4c8328d960b9b71110fa3005ad
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-28 21:36:18 -0300

    restore original LEXOFFICE layout globally

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,5 @@ import App from './App';
 import './styles.css';
 import './legacy-auth.css';
 import './original-layout.css';
-import './sf-brand-theme.css';
-import './converge-original-adaptation.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
