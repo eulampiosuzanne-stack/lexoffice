a698228f867fe2f7c424003eecf3610b4ff92f63
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-28 21:29:58 -0300

    Aplica adaptação visual fiel ao layout original do Converge

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import './styles.css';
 import './legacy-auth.css';
 import './original-layout.css';
 import './sf-brand-theme.css';
+import './converge-original-adaptation.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
