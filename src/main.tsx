99da2c34d0680efaa35bc69be208fa5592ddf5d8
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-08-29 13:09:24 -0300

    feat: activate organization branding bridge

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import OfficeBrandBridge from './OfficeBrandBridge';
 import './styles.css';
 import './legacy-auth.css';
 import './original-layout.css';
 import './interaction-feedback.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App /></BrowserRouter></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <OfficeBrandBridge><App /></OfficeBrandBridge>
+    </BrowserRouter>
+  </React.StrictMode>
+);
