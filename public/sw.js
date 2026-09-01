f99b59118cf033b8ce252a5e5b91313785cb6a36
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-09-01 02:17:47 -0300

    chore(pwa): refresh installed app shell after visual redesign

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='lexoffice-shell-v1';
+const CACHE='lexoffice-shell-v2';
 const SHELL=['/','/manifest.webmanifest','/lexoffice-icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).catch(()=>undefined));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
