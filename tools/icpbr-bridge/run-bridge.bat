faf056578b9738f646cce41cd2ccb3e567921e3b
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-09-03 17:07:48 -0300

    Update run-bridge.bat

--- tools/icpbr-bridge/run-bridge.bat
+++ tools/icpbr-bridge/run-bridge.bat
@@ -1,5 +1,7 @@
 @echo off
-set "PKCS11_DRIVER=C:\Windows\System32\aetpkss1.dll"
+if "%LEXOFFICE_KEYSTORE_TYPE%"=="" set "LEXOFFICE_KEYSTORE_TYPE=WINDOWS-MY"
+set "LEXOFFICE_PKCS11_DLL=C:\Windows\System32\aetpkss1.dll"
 set "LEXOFFICE_ORIGIN=https://lexoffice-ashy.vercel.app"
+echo Modo de certificado: %LEXOFFICE_KEYSTORE_TYPE%
 java -jar lexoffice-icpbr-bridge.jar
 pause
