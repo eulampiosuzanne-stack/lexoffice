f0d343204d20d288079ca91e52812a5664e3cc44
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-09-03 17:05:34 -0300

    Update start-windows.bat

--- tools/icpbr-bridge/start-windows.bat
+++ tools/icpbr-bridge/start-windows.bat
@@ -3,6 +3,8 @@ setlocal
 cd /d "%~dp0"
 set "JAR=target\lexoffice-icpbr-bridge.jar"
 if not exist "%JAR%" (echo Ponte ainda nao compilada. Execute build-windows.bat primeiro.& pause & exit /b 1)
-if not exist "C:\Windows\System32\aetpkss1.dll" (echo SafeSign PKCS11 nao encontrado em C:\Windows\System32\aetpkss1.dll& pause & exit /b 1)
+if "%LEXOFFICE_KEYSTORE_TYPE%"=="" set "LEXOFFICE_KEYSTORE_TYPE=WINDOWS-MY"
+if /I "%LEXOFFICE_KEYSTORE_TYPE%"=="PKCS11" if not exist "%LEXOFFICE_PKCS11_DLL%" if not exist "C:\Windows\System32\aetpkss1.dll" (echo SafeSign PKCS11 nao encontrado em C:\Windows\System32\aetpkss1.dll& pause & exit /b 1)
+echo Modo de certificado: %LEXOFFICE_KEYSTORE_TYPE%
 java -jar "%JAR%"
 pause
