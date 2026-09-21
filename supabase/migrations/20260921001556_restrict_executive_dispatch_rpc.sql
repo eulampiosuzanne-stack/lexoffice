77298a43a57623de65e56d2ba6ad345c7b0527d7
eulampiosuzanne-stack <eulampiosuzanne@gmail.com>
2026-09-20 21:16:18 -0300

    security: restrict executive dispatcher RPCs

---
+++ supabase/migrations/20260921001556_restrict_executive_dispatch_rpc.sql
@@ -0,0 +1,5 @@
+revoke all on function public.lexoffice_executive_cycle(uuid) from public, anon, authenticated;
+revoke all on function public.lexoffice_executive_dispatch(uuid, text) from public, anon, authenticated;
+
+grant execute on function public.lexoffice_executive_cycle(uuid) to service_role;
+grant execute on function public.lexoffice_executive_dispatch(uuid, text) to service_role;
